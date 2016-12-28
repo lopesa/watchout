b51f74e39e0f72097cef5661831b3be9f7c58174
Tony Lopes <tony@lopesdesign.com>
2016-12-27 17:55:07 -0800

    add basic enemy movement

--- client/watchout.js
+++ client/watchout.js
@@ -1,11 +1,15 @@
 // start slingin' some d3 here.
 
 // console.log('my fav way to start');
+// var transition = require('../d3-transition');
+
+var transition = d3.transition();
+
 
 var gameOptions = {
   height: '96%',
   width: '96%',
-  nEnemies: 40,
+  nEnemies: 5,
   padding: 20
 };
 
@@ -19,23 +23,110 @@ var gameBoard = d3.select('.board').append('svg:svg')
   .attr('height', gameOptions.height);
 
 
-var newEnemyPositions = [];
-for (var i = 0; i < gameOptions.nEnemies; i++) {
-  newEnemyPositions.push(
-    {
-      id: i,
-      x: Math.random() * 100,
-      y: Math.random() * 100
-    }
-  );
-}
+var createEnemies = () => {
+  var newEnemyPositions = [];
 
-gameBoard.selectAll('.enemy')
-  .data(newEnemyPositions, d => d.id)
-  .enter()
-  .append('svg:image')
-    .attr('class', 'enemy')
-    .attr('href', 'asteroid.png')
-    .attr('x', enemy => axes.x(enemy.x))
-    .attr('y', enemy => axes.y(enemy.y))
+  for (var i = 0; i < gameOptions.nEnemies; i++) {
+    newEnemyPositions.push(
+      {
+        id: i,
+        x: Math.random() * 100,
+        y: Math.random() * 100
+      }
+    );
+  }
+  return newEnemyPositions;
+};
+
+
+
+
+var tweenWithoutCollisionDetection = (endData) => {
+  // var enemy = d3.select(this);
+  console.log(this);
+  // console.log(context);
+  // console.log(enemy);
+  console.log(endData);
+  // console.log(other);
+  console.log('from in tweenWithoutCollisionDetection');
+  debugger;
+
+
+  var startPos = {
+    x: parseFloat(enemy.attr('x')),
+    y: parseFloat(enemy.attr('y'))
+  };
+
+  var endPos = {
+    x: axes.x(endData.x),
+    y: axes.y(endData.y)
+  };
+
+  return (t) => {
+    var enemyNextPos = {
+      x: startPos.x + (endPos.x - startPos.x) * t,
+      y: startPos.y + (endPos.y - startPos.y) * t
+    };
+
+    enemy.attr('x', enemyNextPos.x)
+      .attr('y', enemyNextPos.y);
+  };
+};
+
+
+
+
+var render = enemyData => {
+  debugger;
+  var enemies = gameBoard.selectAll('.enemy')
+    .data(enemyData, d => d.id);
+  enemies.enter()
+    .append('svg:image')
+      .attr('class', 'enemy')
+      .attr('href', 'asteroid.png')
+      .attr('x', enemy => axes.x(enemy.x))
+      .attr('y', enemy => axes.y(enemy.y));
+
+  // enemies.update()
+  //   .
+
+  enemies.exit()
+    .remove();
+  // console.log(enemies)
+  // console.log('enemies.transition', enemies.transition());
+  // gameBoard.selectAll('.enemy').transition()
+    // .duration(500)
+    // .attr('href', 'grinning-face.png')
+    // .attr('x', '50px')
+  console.log(enemies.transition());
+  enemies.transition()
+    // .tween('custom', tweenWithoutCollisionDetection.bind(this));
+    .tween('custom', function(endData) {
+      console.log('endData', endData);
+      console.log('node', node);
+
+      var node = this;
+      return function(t) {
+        node.setAttribute('x', axes.x(endData.x));
+        node.setAttribute('y', axes.y(endData.y));
+      };
+
+      // tweenWithoutCollisionDetection.call(context);
+    //   console.log('anythihng');
+    });
+};
+
+var gameTurn = () => {
+  var newEnemyPositions = createEnemies();
+  render(newEnemyPositions);
+};
+
+var play = () => {
+  gameTurn();
+  setInterval(gameTurn, 2000);
+};
+
+play();
+
+
 
